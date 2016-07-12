e60c19c3db2a63809b7835179eb95f41f44a21b6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-12 09:38:55 -0400

    Pizza Builder: Change buttons' active state

--- 1-pizza-builder/src/pizza.js
+++ 1-pizza-builder/src/pizza.js
@@ -3,21 +3,26 @@
 $(document).ready(function () {
   $(".btn-pepperonni").on("click", function () {
     $(".pep").toggle();
+    $(".btn-pepperonni").toggleClass("active");
   });
 
   $(".btn-mushrooms").on("click", function () {
     $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
   });
 
   $(".btn-green-peppers").on("click", function () {
     $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
   });
 
   $(".btn-sauce").on("click", function () {
     $(".js-sauce-wrapper").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
   });
 
   $(".btn-crust").on("click", function () {
     $(".js-crust-wrapper").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
   });
 });
