669e0da53ab09dc805ab1af6a2764b9087525017
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-13 09:35:05 -0400

    AJAX: Add appending to list example

--- 2-ajax/app.js
+++ 2-ajax/app.js
@@ -18,5 +18,37 @@ function createCharacter (event) {
 
 
 function fetchCharacters () {
-  alert("FETCH CHARACTERS");
+  var listContent = `
+    <li>
+      <h3> Yoda </h3>
+
+      <ul>
+        <li> Occupation: Grandmaster </li>
+        <li> Weapon: Talking backwards </li>
+      </ul>
+    </li>
+
+    <li>
+      <h3> Obi-Wan Kenobi </h3>
+
+      <ul>
+        <li> Occupation: Liar </li>
+        <li> Weapon: Lies </li>
+      </ul>
+    </li>
+
+    <li>
+      <h3> C-3PO </h3>
+
+      <ul>
+        <li> Occupation: Protocol Droid </li>
+        <li> Weapon: Falling Down </li>
+      </ul>
+    </li>
+  `;
+
+  // $(".js-characters-list").empty();
+  // $(".js-characters-list").append(html);
+
+  $(".js-characters-list").html(listContent);
 }
