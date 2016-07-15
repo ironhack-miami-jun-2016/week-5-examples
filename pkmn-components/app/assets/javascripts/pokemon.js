0aea65313433b45c6c32fa9ef9360713739a214a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-15 11:18:36 -0400

    Pokemon: Add basic details to pokemon component

--- pkmn-components/app/assets/javascripts/pokemon.js
+++ pkmn-components/app/assets/javascripts/pokemon.js
@@ -8,6 +8,37 @@ PokemonApp.Pokemon = class {
 
   render () {
     console.log("Rendering pokemon: #" + this.id);
+
+    $.ajax({
+      type: "GET",
+      url: `/api/pokemon/${this.id}`,
+      success: function (response) {
+        console.log("Pokemon info:");
+        console.log(response);
+
+        $(".js-pkmn-name").text(response.name);
+        $(".js-pkmn-number").text(response.pkdx_id);
+        $(".js-pkmn-height").text(response.height);
+        $(".js-pkmn-weight").text(response.weight);
+        $(".js-pkmn-hp").text(response.hp);
+        $(".js-pkmn-attack").text(response.attack);
+        $(".js-pkmn-defense").text(response.defense);
+        $(".js-pkmn-sp-attack").text(response.sp_atk);
+        $(".js-pkmn-sp-defense").text(response.sp_def);
+        $(".js-pkmn-speed").text(response.speed);
+
+        $(".js-types-list").empty();
+
+        response.types.forEach(function (theType) {
+          var typeHtml = `<li> ${theType.name} </li>`;
+
+          $(".js-types-list").append(typeHtml);
+        });
+
+        $(".js-pokemon-modal").modal("show");
+      },
+      error: PokemonApp.handleError
+    });
   }
 
 }
@@ -20,7 +51,18 @@ PokemonApp.idFromUri = function (pokemonUri) {
                                             // length = 5
   var secondLast = uriSegments.length - 2;  // secondLast = 3
   return uriSegments[secondLast];
-}
+};
+
+
+PokemonApp.handleError = function (error) {
+  console.log(`
+    <guitar riff>
+    Won't you take me to...
+    <guitar riff>
+    ERROR TOWN!?
+  `);
+  console.log(error);
+};
 
 
 $(document).on("ready", function () {
